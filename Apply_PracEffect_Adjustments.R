a30d402eaa54fb120076582300c78aaef66b0aa4
jelman <jelman523@gmail.com>
2016-10-27 13:48:13 -0700

    Adjust using log transformed timing variables

--- Apply_PracEffect_Adjustments.R
+++ Apply_PracEffect_Adjustments.R
@@ -19,30 +19,24 @@
 
 ## Load datasets to be adjusted ##
 
-# Scores that have not been adjusted for AFQT and are on raw test score scale.
-rawscores_df = read.csv("/home/jelman/netshare/K/Projects/PracticeEffects/data/V1V2_PracticeEffect_Raw.csv",
-                        stringsAsFactors = F)
 # Scores that have been adjusted for AFQT and z-scored based on VETSA 1 means and SD's
 nas201adj_df = read.csv("/home/jelman/netshare/K/Projects/PracticeEffects/data/CogData_NAS201TRAN_Adj_Zscored.csv",
                         stringsAsFactors = F)
 
 ## Load calculated practice effects values to adjust by ##
 
-# Values that have been calculated based on scores adjusted for AFQT but on raw test score scale.
-pracEffects_RawScale = read.csv("/home/jelman/netshare/K/Projects/PracticeEffects/results/PracEffects_NAS201TRAN_Results.csv",
-                       row.names=1, stringsAsFactors = F)
+
 # Values that have been calculated based scores adjusted for AFQT and z-scored based on VETSA 1 means and SD's
 pracEffects_Zscored = read.csv("/home/jelman/netshare/K/Projects/PracticeEffects/results/PracEffects_NAS201TRAN_Zscored_Results.csv",
                                 row.names=1, stringsAsFactors = F)
 
 # Get basenames of test scores to adjust
-varNames = c("MR1COR","TRL1T","TRL2T","TRL3T","TRL4T","TRL5T","CSSACC","MTXRAW","CVA1RAW","CVATOT",
-             "CVSDFR","CVLDFR","AFQTPCT","AFQTVOCPCT","AFQTARPCT","AFQTTLPCT","AFQTBXPCT","AFQTPCTTRAN",
-             "AFQTVOCPCTTRAN","AFQTARPCTTRAN","AFQTTLPCTTRAN","AFQTBXPCTTRAN","DSFRAW","DSBRAW",
-             "SSPFRAW","SSPBRAW","LNTOT","LMITOT","LMDTOT","VRITOT","VRDTOT","VRCTOT","HFTOTCOR",
-             "STRWRAW","STRCRAW","STRCWRAW","LFFCOR","LFACOR","LFSCOR","LFCOR","CFANCOR","CFBNCOR",
-             "CFCOR","CSCOR","SRTLMEAN","SRTLSTD","SRTRMEAN","SRTRSTD","SRTGMEAN","SRTGSTD","CHRTLMEAN",
-             "CHRTRMEAN","CHRTLSTD","CHRTRSTD","CHRTGMEAN","CHRTGSTD","RSATOT")
+varNames = c("MR1COR","TRL1TLOG","TRL2TLOG","TRL3TLOG","TRL4TLOG","TRL5TLOG","CSSACC","MTXRAW","CVA1RAW","CVATOT","CVSDFR","CVLDFR",
+             "AFQTPCT","AFQTVOCPCT","AFQTARPCT","AFQTTLPCT","AFQTBXPCT","AFQTPCTTRAN","AFQTVOCPCTTRAN","AFQTARPCTTRAN","AFQTTLPCTTRAN",
+             "AFQTBXPCTTRAN","DSFRAW","DSBRAW","SSPFRAW","SSPBRAW","LNTOT","LMITOT","LMDTOT","VRITOT","VRDTOT","VRCTOT","HFTOTCOR",
+             "STRWRAW","STRCRAW","STRCWRAW","LFFCOR","LFACOR","LFSCOR","LFCOR","CFANCOR","CFBNCOR","CFCOR","CSCOR","SRTLMEANLOG",
+             "SRTLSTDLOG","SRTRMEANLOG","SRTRSTDLOG","SRTGMEANLOG","SRTGSTDLOG","CHRTLMEANLOG","CHRTRMEANLOG","CHRTLSTDLOG",
+             "CHRTRSTDLOG","CHRTGMEANLOG","CHRTGSTDLOG","RSATOT")
 
 
 #-------------------------------------------#
@@ -62,25 +56,6 @@ for (varName in varNames) {
 }
 
 # Save out dataset that has been adjusted for AFQT and Practice Effects, as well as z-scored based on VETSA 1 values 
-write.csv(nas201adj_PEadj, '/home/jelman/netshare/K/Projects/PracticeEffects/data/CogData_NAS201TRAN_PracEffect_Adj_Zscored.csv',
-          row.names=FALSE)
-
-############################################
-
-# Apply practice effect adjustment to raw scores.
-# Z-score is converted to raw score scale based on mean and SD of scores that have had 
-# age 20 AFQT regressed out. 
-
-rawscore_PEadj = rawscores_df
-idxV1V2 = which(rawscore_PEadj$VETSAGRP=="V1V2")
-for (varName in varNames) {
-  varName_V2 = paste0(varName, "_V2")
-  varName_PE = varName
-  pe_RawScale = pracEffects_RawScale[varName_PE,"PracticeEffect"]
-  rawscore_PEadj[idxV1V2, varName_V2] = rawscore_PEadj[idxV1V2, varName_V2] - pe_RawScale
-}
-
-# Save out dataset that has been adjusted for AFQT and Practice Effects, and is kept on raw test score scale 
-write.csv(rawscore_PEadj, '/home/jelman/netshare/K/Projects/PracticeEffects/data/CogData_NAS201TRAN_PracEffect_Adj.csv',
+write.csv(nas201adj_PEadj, '/home/jelman/netshare/K/Projects/PracticeEffects/results/CogData_NAS201TRAN_PracEffect_Adj_Zscored.csv',
           row.names=FALSE)
 
