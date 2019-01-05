dd0e9d174e8f534bdb7a1a024d4ee05cab4d7628
jelman <jelman523@gmail.com>
2019-01-04 16:19:55 -0800

    Create PE adjusted scores for each follow-up group. Does not fill in replacement scores in these new variables

--- 3_Apply_PracEffect_Adjustments.R
+++ 3_Apply_PracEffect_Adjustments.R
@@ -16,15 +16,41 @@ library(dplyr)
 #     Define variables names and functions    #
 ###############################################
 
+
 # Function to replace negative numbers with zero
 zeroFloor = function(x){
   x = (x + abs(x)) / 2
   x}
 
+
 # Function to replace times over a given cutoff
 timeCeiling = function(x, maxt){
   x[x>log(maxt) & !is.na(x)] = log(maxt)
   x
+}
+
+
+# Calculate standard errors for practice effects using bootstrap resampling
+# Inputs:
+# ---------
+# unadj_df: dataframe with variables to be adjusted 
+# pracEffects: dataframe with practice effect values. Row names should contain variable names to adjust
+# idx: Indices of returnees to whose scores should be adjusted
+# colSuffix: Column name suffix denoting correct timepoint to apply adjustment to
+#
+# Returns:
+# ----------
+# unadj_df_PEadj: dataset with practice effect corrected variables included
+applyPEadjustment = function(unadj_df, pracEffects, idx, colSuffix){
+  varNames = row.names(pracEffects)
+  unadj_df_PEadj = unadj_df
+  for (varName in varNames) {
+    varName_t2 = paste0(varName, colSuffix)
+    newVarName_t2 = paste0(varName_t2,"p")
+    peVal = pracEffects[varName,"PracticeEffect"]
+    unadj_df_PEadj[idx, newVarName_t2] = unadj_df_PEadj[idx, varName_t2] - peVal
+  }
+  unadj_df_PEadj
 }
 
 
@@ -37,22 +63,53 @@ timeCeiling = function(x, maxt){
 # Load dataset to be adjusted: No age 20 afqt adjustment, raw score scale
 unadj_df = read.csv("~/netshare/M/PSYCH/KREMEN/VETSA DATA FILES_852014/a_Practice effect revised cog scores/Practice Effect Cognition/VETSA 3/data/V1V2V3_CogData_Unadj.csv",
                     stringsAsFactors = F)
-
-# Load practice effect values calculated based scores adjusted for AFQT but on raw score scale
-pracEffects = read.csv("~/netshare/M/PSYCH/KREMEN/Practice Effect Cognition/results/PracEffectValues_NASAdj.csv",
-                       row.names=1, stringsAsFactors = F)
-
-# Get basenames of test scores to adjust
-varNames = row.names(pracEffects)
 unadj_df_PEadj = unadj_df
-idxV1V2 = which(unadj_df_PEadj$VETSAGRP=="V1V2")
-for (varName in varNames) {
-  varName_V2 = paste0(varName, "_V2")
-  newVarName_V2 = paste0(varName_V2,"p")
-  peVal = pracEffects[varName,"PracticeEffect"]
-  unadj_df_PEadj[, newVarName_V2] = unadj_df_PEadj[, varName_V2]
-  unadj_df_PEadj[idxV1V2, newVarName_V2] = unadj_df_PEadj[idxV1V2, newVarName_V2] - peVal
-}
+
+#---------------------------------#
+#     V1V2 & V1V2V3: t1 -> t2     #
+#---------------------------------#
+pracEffects_V1V2_V1V2V3_t1t2 = read.csv("/home/jelman/netshare/M/PSYCH/KREMEN/VETSA DATA FILES_852014/a_Practice effect revised cog scores/Practice Effect Cognition/VETSA 3/results/PracEffects_NAS201TRAN_V1V2-V1V2V3-t1t2.csv",
+                       row.names=1, stringsAsFactors = F)
+idxV1V2_V1V2V3 = which(unadj_df$VETSAGRP %in% c("V1V2", "V1V2V3"))
+unadj_df_PEadj = applyPEadjustment(unadj_df_PEadj, pracEffects_V1V2_V1V2V3_t1t2, idxV1V2_V1V2V3, "_V2")
+
+
+#---------------------------------#
+#        V1V2V3: t2 -> t3         #
+#---------------------------------#
+pracEffects_V1V2V3_t2t3 = read.csv("/home/jelman/netshare/M/PSYCH/KREMEN/VETSA DATA FILES_852014/a_Practice effect revised cog scores/Practice Effect Cognition/VETSA 3/results/PracEffects_NAS201TRAN_V1V2V3-t2t3.csv",
+                                        row.names=1, stringsAsFactors = F)
+idxV1V2V3 = which(unadj_df$VETSAGRP %in% c("V1V2V3"))
+unadj_df_PEadj = applyPEadjustment(unadj_df_PEadj, pracEffects_V1V2V3_t2t3, idxV1V2V3, "_V3")
+
+
+#---------------------------------#
+#        V2V3: t2 -> t3           #
+#---------------------------------#
+pracEffects_V2V3_t2t3 = read.csv("/home/jelman/netshare/M/PSYCH/KREMEN/VETSA DATA FILES_852014/a_Practice effect revised cog scores/Practice Effect Cognition/VETSA 3/results/PracEffects_NAS201TRAN_V2V3-t2t3.csv",
+                                   row.names=1, stringsAsFactors = F)
+idxV2V3 = which(unadj_df$VETSAGRP %in% c("V2V3"))
+unadj_df_PEadj = applyPEadjustment(unadj_df_PEadj, pracEffects_V2V3_t2t3, idxV2V3, "_V3")
+
+
+#---------------------------------#
+#        V1V3: t1 -> t3           #
+#---------------------------------#
+pracEffects_V1V3_t1t3 = read.csv("/home/jelman/netshare/M/PSYCH/KREMEN/VETSA DATA FILES_852014/a_Practice effect revised cog scores/Practice Effect Cognition/VETSA 3/results/PracEffects_NAS201TRAN_V2V3-t2t3.csv",
+                                 row.names=1, stringsAsFactors = F)
+idxV1V3 = which(unadj_df$VETSAGRP %in% c("V1V3"))
+unadj_df_PEadj = applyPEadjustment(unadj_df_PEadj, pracEffects_V1V3_t1t3, idxV1V3, "_V3")
+
+
+#---------------------------------#
+#        V1neV3: t1 -> t2         #
+#---------------------------------#
+# V1ne subjects are treated like V1V2 subjects for the purposes of correction. Their t1 and t2 
+# assessments took place at V2 and V3, respectively. 
+idxV1neV3 = which(unadj_df$VETSAGRP %in% c("V1neV3"))
+unadj_df_PEadj = applyPEadjustment(unadj_df_PEadj, pracEffects_V1V2_V1V2V3_t1t2, idxV1neV3, "_V3")
+
+
 
 # Replace invalid negative numbers with 0
 negVars = names(unadj_df_PEadj)[grepl("TRAN", names(unadj_df_PEadj)) | grepl("DPRIME", names(unadj_df_PEadj))]
@@ -66,5 +123,5 @@ trl150vars = names(unadj_df_PEadj)[grepl("TRL[1235]T", names(unadj_df_PEadj))]
 unadj_df_PEadj = unadj_df_PEadj %>% mutate_at(.cols=trl150vars, .funs=timeCeiling, maxt=150)
 
 # Save out dataset of scores not adjusted for AFQT, raw score scale, practice effect adjusted
-write.csv(unadj_df_PEadj, '~/netshare/M/PSYCH/KREMEN/Practice Effect Cognition/data/V1V2_CogData_PE.csv',
+write.csv(unadj_df_PEadj, '~/netshare/M/PSYCH/KREMEN/VETSA DATA FILES_852014/a_Practice effect revised cog scores/Practice Effect Cognition/VETSA 3/data/V1V2V3_CogData_PE.csv',
           row.names=FALSE)
